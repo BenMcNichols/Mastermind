c790f96c76015c9b97bd38041068be68c03e508a
BenMcNichols <46431092+BenMcNichols@users.noreply.github.com>
2021-08-16 21:22:28 -0500

    Update MastermindGame.py
    Added more game dialouge and improved logic - still an issue with the last peg though.

--- MastermindGame.py
+++ MastermindGame.py
@@ -10,7 +10,7 @@ import time
 
 # New Game Setup
 def newGame():
-    print("ENTER SEQUENCE")
+    print("ENTER 4-PEG SEQUENCE")
     print("COLORS: A,B,C,D,E,F")
     secretSequence = []
     for i in range(4):
@@ -23,70 +23,111 @@ def newGame():
 
 # Generaete a new guess which meets the rules
 def generateGuess(turnNumber,guessHistory, responseHistory):
-    guessResponseHistory = [] # list to mimic response history
-    for cluePair in range(turnNumber):
-        blackTestList = [] #if peg would be black, add peg index to list
-        whiteTestList = [] # if peg would be white, add peg to list (Not index?)
-        print("Begin Black Checking")
-        for peg in range(len(guessSequence)):
-            print("Peg = ",peg)
-            if guessSequence[peg]==guessHistory[cluePair][peg]:
-                
-                print(guessSequence[peg],guessHistory[cluePair][peg])
-                print("BlackDing")
-                blackTestList.append(peg) 
-                
-        print("Conclusion: Black", len(blackTestList))
-        
-        #remove black pegs from guess & historical list
-        TempHistList = guessHistory[cluePair][:] # temp list to remove values from
-        TempGuessList = guessSequence[:] # temp list to remove values from
-        for blackpeg in blackTestList[::-1]:
-            del TempHistList[blackpeg]
-            del TempGuessList[blackpeg]
-        print("Remaining Guess List",TempGuessList)
-        
-        print("Begin White Checking")
-        WhiteTempHistList = TempHistList[:]
-        matchHistList = []
-        matchGuessList = []
-        for i in range(len(TempHistList)):
-            for j in range(len(TempGuessList)):
-                print("i,J=",i,j)
-                if TempHistList[i] == TempGuessList[j]:
-                    if i not in matchHistList and j not in matchGuessList:
-                        whiteTestList.append(TempHistList[i])
-                    matchHistList.append(i)
-                    matchGuessList.append(j)
+    print(turnNumber,guessHistory,responseHistory)
+    colorList = ["A", "B", "C", "D","E"]
+    #First, iterate through possible answers
+    viableGuesses = [] # list of viable guesses
+    for position1 in colorList:
+        for position2 in colorList:
+            for position3 in colorList:
+                for position4 in colorList:
+                    guessSequence = [position1, position2, position3, position4]
+                    if guessSequence in guessHistory:
+                        continue
+                    guessResponseHistory = [] # list to mimic response history
+                    #print(guessSequence)
+                    for cluePair in range(turnNumber):
+                        blackTestList = [] #if peg would be black, add peg index to list
+                        whiteTestList = [] # if peg would be white, add peg to list (Not index?)
+                        #print("Begin Black Checking")
+                        for peg in range(len(guessSequence)):
+                            #print("Peg = ",peg)
+                            if guessSequence[peg]==guessHistory[cluePair][peg]:
+                                
+                                #print(guessSequence[peg],guessHistory[cluePair][peg])
+                                #print("BlackDing")
+                                blackTestList.append(peg) 
+                                
+                        #print("Conclusion: Black", len(blackTestList))
+                        
+                        #remove black pegs from guess & historical list
+                        TempHistList = guessHistory[cluePair][:] # temp list to remove values from
+                        TempGuessList = guessSequence[:] # temp list to remove values from
+                        for blackpeg in blackTestList[::-1]:
+                            del TempHistList[blackpeg]
+                            del TempGuessList[blackpeg]
+                        #print("Remaining Guess List",TempGuessList)
+                        
+                        #print("Begin White Checking")
+                        WhiteTempHistList = TempHistList[:]
+                        matchHistList = []
+                        matchGuessList = []
+                        for i in range(len(TempHistList)):
+                            for j in range(len(TempGuessList)):
+                                #print("i,J=",i,j)
+                                if TempHistList[i] == TempGuessList[j]:
+                                    if i not in matchHistList and j not in matchGuessList:
+                                        whiteTestList.append(TempHistList[i])
+                                    matchHistList.append(i)
+                                    matchGuessList.append(j)
+                                    
+                        #print("### Analysis Complete ###")
+                        #print("Black Peg Count:",len(blackTestList))
+                        #print("White Peg Count:",len(whiteTestList))
+                        guessResponseHistory.append([len(blackTestList),len(whiteTestList)])    
+                    #print(guessResponseHistory)
+                    # is the guess viable when compared to previous answers?
+                    if guessResponseHistory == responseHistory:
+                        #print("This is a good guess to make:", guessSequence)
+                        viableGuesses.append(guessSequence)
+                        break
+    
+    #print the final viable guess list:
+    print("Guess Options:",viableGuesses)
+    return(viableGuesses)
                     
-        print("### Analysis Complete ###")
-        print("Black Peg Count:",len(blackTestList))
-        print("White Peg Count:",len(whiteTestList))
-        guessResponseHistory.append([len(blackTestList),len(whiteTestList)])    
-    
-    # is the guess viable when compared to previous answers?
-    if guessResponseHistory == responseHistory:
-        print("This is a good guess to make:", guessSequence)
-        return True
-            
-                
-            
-        
-    
-    #Then, check if answer matches previous responses
 
-# Check if the guess matches the 
+# Check if the guess matches the existing chips
 def checkGuess():
     return
     
 ''''Actual program starts here'''
 
-secretSequence = ["A","C","B","D"] #newGame()
+secretSequence = newGame()
 
-guessHistory = [["A","B","C","D"]]
-responseHistory = [[2,2]]
+guessHistory = []
+
+responseHistory = []
+
+turnNumber = 0 #Turn 1
 
 for turnNumber in range(8):
     print("Turn #", turnNumber+1)
-    generateGuess(turnNumber,guessHistory,responseHistory)
+    viableGuesses = generateGuess(turnNumber,guessHistory,responseHistory)
+    
+    print("Number of options = ",len(viableGuesses))
+    if len(viableGuesses)>0:
+        myGuess = viableGuesses[0]
+        print("My Guess Is:", myGuess)
+        guessHistory.append(myGuess)
+        print("(Secret Sequence is:", secretSequence)
+        print("Number of Black:")
+        blackPegCount = int(input())
+        print("Number of White Pegs")
+        whitePegCount = int(input())
+        responseHistory.append([blackPegCount,whitePegCount])
+        
+        #Win Condition
+        if blackPegCount == 4:
+            print("I Won in ",turnNumber+1,"Turns!")
+            break
+        
+    else:
+        print("I give up! You win!")
+        break
+        
+
+
+        
+    
     
